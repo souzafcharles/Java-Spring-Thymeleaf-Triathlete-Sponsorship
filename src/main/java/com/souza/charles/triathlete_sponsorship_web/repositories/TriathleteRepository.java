f8ca76db0a558995f234da0636d9cba3cfbd455f
souzafcharles <souzach@outlook.com>
2025-02-27 10:32:03 -0300

    feat: add search functionality for triathletes by name

--- src/main/java/com/souza/charles/triathlete_sponsorship_web/repositories/TriathleteRepository.java
+++ src/main/java/com/souza/charles/triathlete_sponsorship_web/repositories/TriathleteRepository.java
@@ -3,5 +3,10 @@ package com.souza.charles.triathlete_sponsorship_web.repositories;
 import com.souza.charles.triathlete_sponsorship_web.models.Triathlete;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TriathleteRepository extends JpaRepository<Triathlete, Long> {
+
+    List<Triathlete> findByNameContainingIgnoreCase(String name);
+
 }
